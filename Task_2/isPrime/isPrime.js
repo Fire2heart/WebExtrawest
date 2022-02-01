56f8d436318d22f9baa8647650228d7cff146749
Fire2heart <52fire2heart52@gmail.com>
2022-02-01 12:20:00 +0200

    Hot fix

--- Task_2/isPrime/isPrime.js
+++ Task_2/isPrime/isPrime.js
@@ -3,16 +3,3 @@ export function isPrime(num) {
         if (num % i === 0) return false;
     return num > 1;
 }
-
-// function isPrime(num) {
-//     var sqrtnum = Math.floor(Math.sqrt(num));
-//     var prime = num != 1;
-//     for (var i = 2; i < sqrtnum + 1; i++) { // sqrtnum+1
-//         if (num % i == 0) {
-//             prime = false;
-//             break;
-//         }
-//     }
-//     return prime;
-// }
-console.log(isPrime(-1))
